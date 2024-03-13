702c910e5bc948531cf93bed6c3b51656aed3591
mormiddle <mormiddle@qq.com>
2024-03-13 16:29:39 +0800

    2024年3月13日16:29:42

---
+++ TreeStructuresStudy_C/0604Tree.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define TElemType int
+
+typedef struct BiTNode
+{
+    TElemType data;
+    struct BiTNode *lchild, *rchild;
+    struct BiTNode *parent;
+}BiTNode, *BiTree;
+
+void CreateBiTree(BiTree *T)
+{
+    *T = (BiTNode*)malloc(sizeof(BiTNode));
+    (*T)->data = 1;
+    (*T)->lchild = (BiTNode*)malloc(sizeof(BiTNode));
+    (*T)->lchild->data = 2;
+    (*T)->rchild = (BiTNode*)malloc(sizeof(BiTNode));
+    (*T)->rchild->data = 3;
+    (*T)->rchild->lchild = NULL;
+    (*T)->rchild->rchild = NULL;
+    (*T)->lchild->lchild = (BiTNode*)malloc(sizeof(BiTNode));
+    (*T)->lchild->lchild->data = 4;
+    (*T)->lchild->rchild = NULL;
+    (*T)->lchild->lchild->lchild = NULL;
+    (*T)->lchild->lchild->rchild = NULL;
+}
+
+
+int main() {
+    BiTree Tree;
+    CreateBiTree(&Tree);
+    printf("%d", Tree->lchild->lchild->data);
+    
+    getchar();
+    return 0;
+}
---
+++ TreeStructuresStudy_C/0606Tree01.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define TElemType int
+
+typedef struct BiTNode
+{
+    TElemType data;
+    struct BiTNode *lchild, *rchild;
+} BiTNode, *BiTree;
+
+void CreateBiTree(BiTree *T)
+{
+    *T = (BiTNode *)malloc(sizeof(BiTNode));
+    (*T)->data = 1;
+    (*T)->lchild = (BiTNode *)malloc(sizeof(BiTNode));
+    (*T)->rchild = (BiTNode *)malloc(sizeof(BiTNode));
+
+    (*T)->lchild->data = 2;
+    (*T)->lchild->lchild = (BiTNode *)malloc(sizeof(BiTNode));
+    (*T)->lchild->rchild = (BiTNode *)malloc(sizeof(BiTNode));
+    (*T)->lchild->rchild->data = 5;
+    (*T)->lchild->rchild->lchild = NULL;
+    (*T)->lchild->rchild->rchild = NULL;
+    (*T)->rchild->data = 3;
+    (*T)->rchild->lchild = (BiTNode *)malloc(sizeof(BiTNode));
+    (*T)->rchild->lchild->data = 6;
+    (*T)->rchild->lchild->lchild = NULL;
+    (*T)->rchild->lchild->rchild = NULL;
+    (*T)->rchild->rchild = (BiTNode *)malloc(sizeof(BiTNode));
+    (*T)->rchild->rchild->data = 7;
+    (*T)->rchild->rchild->lchild = NULL;
+    (*T)->rchild->rchild->rchild = NULL;
+    (*T)->lchild->lchild->data = 4;
+    (*T)->lchild->lchild->lchild = NULL;
+    (*T)->lchild->lchild->rchild = NULL;
+}
+
+void displayElem(BiTNode* elem)
+{
+    printf("%d ", elem->data);
+}
+
+void PreOrderTraverse(BiTree T)
+{
+    if (T) 
+    {
+        displayElem(T);
+        PreOrderTraverse(T->lchild);
+        PreOrderTraverse(T->rchild);
+    }
+
+    return;
+}
+
+int main()
+{
+    BiTree Tree;
+    CreateBiTree(&Tree);
+    printf("先序遍历：\n");
+    PreOrderTraverse(Tree);
+    
+    getchar();
+    return 0;
+}
